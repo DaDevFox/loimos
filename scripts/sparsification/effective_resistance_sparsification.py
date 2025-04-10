e94e02220e4553c16f5f593e01757c8e74558fe0
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-04-09 22:23:39 -0400

    fix: clean edge_list for NAs and int format

--- scripts/sparsification/effective_resistance_sparsification.py
+++ scripts/sparsification/effective_resistance_sparsification.py
@@ -107,7 +107,7 @@ def parse_args():
 
 def process_subset(gpu_id, df_subset, q, epsilon=0.1, method='kts'):
     with cuda.Device(gpu_id):
-        edge_list = df_subset[['pid', 'lid']].to_numpy()  # should be 2 x m shape
+        edge_list = df_subset[['pid', 'lid']].dropna().astype(int).to_numpy()  # should be 2 x m shape
         weights = df_subset['duration'].to_numpy()  # weight edge by visit duration
 
         # Time the Network constructor
